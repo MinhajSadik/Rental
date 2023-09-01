3bb5dd91ef64ee9801fcaa5f0869be9dc4324c64
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-09-02 01:29:17 +0600

    Nestjs, Prisma and Postgresql setup completed

---
+++ backend/prisma/migrations/20230901192757_user_model_created/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "idCardFront" TEXT NOT NULL,
+    "idCardBack" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
